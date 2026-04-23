f3354209ce8a90177d640f781c9d4be6c759b64a
karatalov <nursultankaratalov2@gmail.com>
2026-04-23 21:26:02 +0600

    FIX PRISMA 5

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
 
-const adapter = new PrismaPg({
-	connectionString: process.env.DATABASE_URL,
-})
-
-const prismaClientSingleton = () => {
-	return new PrismaClient({ adapter })
+const globalForPrisma = globalThis as unknown as {
+	prisma: PrismaClient | undefined
 }
 
-declare const globalThis: {
-	prismaGlobal: ReturnType<typeof prismaClientSingleton>
-} & typeof global
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-const prisma = globalThis.prismaGlobal || prismaClientSingleton()
-export default prisma
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== 'production') {
+	globalForPrisma.prisma = prisma
+}
