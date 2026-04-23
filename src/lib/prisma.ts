3c811ff2c75f6c04910996b4e27533975300e855
karatalov <nursultankaratalov2@gmail.com>
2026-04-23 20:53:16 +0600

    FIX PRISMA

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,17 @@
-import { PrismaClient } from 'generated/prisma'
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 const globalForPrisma = globalThis as unknown as {
-	prisma: PrismaClient
+	prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma =
+	globalForPrisma.prisma ??
+	new PrismaClient({
+		adapter: new PrismaPg({
+			connectionString: process.env.DATABASE_URL!,
+		}),
+	})
 
 if (process.env.NODE_ENV !== 'production') {
 	globalForPrisma.prisma = prisma
